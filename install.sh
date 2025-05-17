85a2a0c526ebe24996cf1e2d1ac83b33c8e3029d
Christian Zufferey <christian@zufferey.com>
2025-05-17 23:43:33 +0200

    new: ça commence à tourner, mais j'ai trop de choses à faire encore manuellement

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#Petit script pour installer facilement une ou plusieurs instances d'un broker MQTT
+# zf250517.2244, zf250517.2244
+
+# source: https://github.com/sukesh-ak/setup-mosquitto-with-docker
+
+touch mqtt-1/config/pwfile
+
+
+
