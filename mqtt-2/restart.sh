d3aa98648c0fc364ecc12846c6d7abd19c0347f1
Christian Zufferey <christian@zufferey.com>
2025-05-18 17:52:02 +0200

    new: ajouté une 2e instance \o/

---
+++ mqtt-2/restart.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#Petit script pour redémarrer facilement l'instance MQTT
+# zf250517.2244, zf250518.0006
+
+# source: https://github.com/sukesh-ak/setup-mosquitto-with-docker
+
+docker compose restart
---
+++ mqtt-2/start.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#Petit script pour démarrer facilement l'instance MQTT
+# zf250517.2244, zf250518.1529
+
+# source: https://github.com/sukesh-ak/setup-mosquitto-with-docker
+
+
+sudo groupadd -g 1883 mqtt
+sudo useradd -u 1883 -g mqtt mqtt
+sudo usermod -aG mqtt ubuntu
+
+sudo touch ./config/pwfile
+sudo chmod 0700 ./config/pwfile
+
+docker compose up -d
+
+echo -e "
+
+Noubliez pas de créer un user avec:
+
+./create_user.sh
+
+
+"
+
