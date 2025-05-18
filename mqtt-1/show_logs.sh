4126de88f8b781edeab5104192ea6d780381de73
Christian Zufferey <christian@zufferey.com>
2025-05-18 17:19:35 +0200

    new: ajouté un script pour afficher facilement les logs de mqtt

---
+++ mqtt-1/show_logs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#Petit script pour démarrer facilement l'instance MQTT
+# zf250517.2244, zf250518.1716
+
+# source: https://github.com/sukesh-ak/setup-mosquitto-with-docker
+
+
+
+docker compose logs -f
+
+
